fc6a06f8f8db48c21c8eecc41565c4713675123c
pendevx <guangheng1234@gmail.com>
2020-08-01 23:43:07 +1200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VB_to_Cambridge_Pseudocode_Transpiler
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+            this.WindowStyle = WindowStyle.SingleBorderWindow;
+        }
+
+        private void transpileButton_Click(object sender, RoutedEventArgs e)
+        {
+            string input = inputTextbox.Text;
+            Parser parser = new Parser(input);
+            outputTextbox.Text = parser.Parse();
+        }
+    }
+}
